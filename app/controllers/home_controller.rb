a9e6041dfbdfe60c1b7396e7878e9cd30c121edf
MargVander <margauxvanderbeken@gmail.com>
2019-06-21 00:18:56 +0200

    changed method to know closest stations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,24 @@
 class HomeController < ApplicationController
   def index
     thp_location = { "latitude" => 48.872155, "longitude" => 2.346668 }
-    @station_with_bikes = Station.new.bikes["network"]["stations"].select { |station| station["free_bikes"].positive? }
-    @closest_bikes_stations = @station_with_bikes.min_by(5) { |x| (x["latitude"].to_f - thp_location["latitude"]).abs + (x["longitude"].to_f - thp_location["longitude"]).abs }
-    # render json: @bikes
-    # xs.min_by { |x| (x.to_f - 82.4).abs }
+    @station_with_bikes = BikeStation.where("free_bikes != 0" )
+    @closest_bikes_stations = @station_with_bikes.min_by(5) { |x| dist([thp_location["latitude"].to_f, thp_location["longitude"].to_f], [x.latitude, x.longitude]) }
+  end
+
+  def dist(loc1, loc2)
+    rad_per_deg = Math::PI / 180
+    rkm = 6371
+    rm = rkm * 1000
+
+    dlat_rad = (loc2[0] - loc1[0]) * rad_per_deg
+    dlon_rad = (loc2[1] - loc1[1]) * rad_per_deg
+
+    lat1_rad, _lon1_rad = loc1.map { |i| i * rad_per_deg }
+    lat2_rad, _lon2_rad = loc2.map { |i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad / 2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad / 2)**2
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    rm * c
   end
 end
