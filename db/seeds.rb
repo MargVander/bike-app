eb7b5ab95443d67e55482a60ce78310d026983a3
MargVander <margauxvanderbeken@gmail.com>
2019-06-21 00:24:00 +0200

    add stations from api in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Station.new.bikes["network"]["stations"].each do |station|
+  BikeStation.create(name: station["name"], free_bikes: station["free_bikes"], latitude: station["latitude"], longitude: station["longitude"])
+end
